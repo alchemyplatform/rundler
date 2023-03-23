46124746de8f449f0e29742a8980a233eb2614e0
dancoombs <dan.coombs@alchemy.com>
2023-03-23 12:38:49 -0400

    chore(test): add scripts for local spec testing

---
+++ test/spec-tests/local/run-spec-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+cd "$(dirname "$0")"
+
+# ASSUMES THAT YOU'VE INSTALLED THE DEPENDENCIES FOR THE SPEC TESTS
+# (cd bundler-spec-tests && pdm install && pdm run update-deps)
+
+cargo build
+
+./launcher.sh stop # kill already running processes if any
+./launcher.sh start
+
+sleep 5
+
+(cd ../bundler-spec-tests && pdm run pytest -rA -W ignore::DeprecationWarning --url http://localhost:3000 --entry-point 0x0576a174D229E3cFA37253523E645A78A0C91B57 --ethereum-node http://localhost:8545 $@)
+
+./launcher.sh stop
