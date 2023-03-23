99fcb63282f94e1fcb7e3b16a5db3815321a58c1
dancoombs <dan.coombs@alchemy.com>
2023-03-23 08:11:29 -0400

    fix(test): stop docker always in run-spec-tests

--- test/spec-tests/run-spec-tests.sh
+++ test/spec-tests/run-spec-tests.sh
@@ -7,3 +7,5 @@ cd "$(dirname "$0")"
 docker build ../.. -t alchemy-platform/alchemy-bundler:latest
 
 cd bundler-spec-tests && pdm run test --launcher-script=../launchers/alchemy-bundler-launcher/alchemy-bundler-launcher.sh $@
+
+../launchers/alchemy-bundler-launcher/alchemy-bundler-launcher.sh stop
