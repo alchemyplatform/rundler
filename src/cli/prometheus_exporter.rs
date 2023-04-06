063a3b9abbf5eaad032b9f654e53db06b3e31dcc
dancoombs <dan.coombs@alchemy.com>
2023-04-06 12:11:26 -0400

    feat(metrics): add process metrics

--- src/cli/prometheus_exporter.rs
+++ src/cli/prometheus_exporter.rs
@@ -1,7 +1,8 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
 use itertools::Itertools;
 use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_process::Collector;
 use metrics_util::layers::{PrefixLayer, Stack};
 
 pub fn initialize<'a>(
@@ -23,6 +24,14 @@ pub fn initialize<'a>(
     Stack::new(recorder)
         .push(PrefixLayer::new("rundler"))
         .install()?;
+
+    tokio::spawn(async {
+        let collector = Collector::default();
+        loop {
+            collector.collect();
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    });
 
     Ok(())
 }
