87b9877aab7b1f0d8e0529b2a1b1f3d8ce3cef1d
dancoombs <dan.coombs@alchemy.com>
2023-04-06 10:15:32 -0400

    feat(metrics): add tags to metrics

--- src/cli/prometheus_exporter.rs
+++ src/cli/prometheus_exporter.rs
@@ -1,12 +1,24 @@
 use std::net::SocketAddr;
 
+use itertools::Itertools;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use metrics_util::layers::{PrefixLayer, Stack};
 
-pub fn initialize(listen_addr: SocketAddr) -> anyhow::Result<()> {
-    let (recorder, exporter) = PrometheusBuilder::new()
-        .with_http_listener(listen_addr)
-        .build()?;
+pub fn initialize<'a>(
+    listen_addr: SocketAddr,
+    tags: impl IntoIterator<Item = &'a String>,
+) -> anyhow::Result<()> {
+    let mut builder = PrometheusBuilder::new().with_http_listener(listen_addr);
+
+    let tags: Vec<(&str, &str)> = tags
+        .into_iter()
+        .filter_map(|t| t.split('=').collect_tuple())
+        .collect();
+    for (k, v) in tags {
+        builder = builder.add_global_label(k, v);
+    }
+
+    let (recorder, exporter) = builder.build()?;
     tokio::spawn(exporter);
     Stack::new(recorder)
         .push(PrefixLayer::new("rundler"))
