01bc71c48a872642ffdfcd760c8352ab5a8ff015
moldy <michael.moldoveanu@alchemy.com>
2023-04-10 19:55:54 -0400

    feat: update to latest version of EP

--- src/bin/send_ops.rs
+++ src/bin/send_ops.rs
@@ -15,7 +15,7 @@ async fn main() -> anyhow::Result<()> {
     // simply call the nonce method multiple times
     for i in 0..10 {
         println!("Sending op {i}");
-        let op = clients.new_wallet_op(wallet.nonce(), 0.into()).await?;
+        let op = clients.new_wallet_op(wallet.get_nonce(), 0.into()).await?;
         let call = entry_point.handle_ops(vec![op], bundler_client.address());
         eth::await_mined_tx(call.send(), "send user operation").await?;
     }
