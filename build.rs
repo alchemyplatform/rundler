edfcc8cbd9a0622188f60e39139ff58acc6dd822
noam <noam@alchemyapi.io>
2023-08-21 11:44:13 -0400

    chore(protolint): lint proto files, add comments, and add linter

--- build.rs
+++ build.rs
@@ -58,10 +58,10 @@ fn generate_protos() -> Result<(), Box<dyn error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("op_pool_descriptor.bin"))
-        .compile(&["proto/op_pool.proto"], &["proto"])?;
+        .compile(&["proto/op_pool/op_pool.proto"], &["proto"])?;
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("builder_descriptor.bin"))
-        .compile(&["proto/builder.proto"], &["proto"])?;
+        .compile(&["proto/builder/builder.proto"], &["proto"])?;
     Ok(())
 }
 
