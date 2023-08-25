32c903cb23cf92787e6f9511d4475f4282338501
0xfourzerofour <joshpauline@gmail.com>
2023-08-25 14:28:18 -0400

    feat(ci): test with localhost network

--- test/spec-tests/launchers/rundler-launcher/rundler-launcher.sh
+++ test/spec-tests/launchers/rundler-launcher/rundler-launcher.sh
@@ -12,7 +12,7 @@ case $1 in
  start)
 	docker-compose up -d --wait
 	cast send --from $(cast rpc eth_accounts | tail -n 1 | tr -d '[]"') --value 1ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 > /dev/null
-    cd ../../bundler-spec-tests/@account-abstraction && yarn deploy --network 127.0.0.1
+    cd ../../bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
 	;;
  stop)
  	docker-compose down -t 3
