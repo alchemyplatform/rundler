4e8b388d41c5b00eae0560f8517561c8f958d1d6
0xfourzerofour <joshpauline@gmail.com>
2023-08-28 12:05:18 -0400

    feat(ci): add unlocked flag to ci

--- test/spec-tests/launchers/rundler-launcher/rundler-launcher.sh
+++ test/spec-tests/launchers/rundler-launcher/rundler-launcher.sh
@@ -10,7 +10,7 @@ case $1 in
 	;;
 
  start)
-	cast send --from $(cast rpc eth_accounts | tail -n 1 | tr -d '[]"') --value 1ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 > /dev/null
+	cast send --from $(cast rpc eth_accounts | tail -n 1 | tr -d '[]"') --unlocked --value 1ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 > /dev/null
     cd ../../bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
 	;;
  stop)
