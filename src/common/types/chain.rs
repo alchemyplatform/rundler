c553ebc93addb54ce2f159de7f163e4ff7ab9321
0xfourzerofour <joshpauline@gmail.com>
2023-10-31 14:45:03 -0400

    feat(arb-sepolia): add sepolia chain ID

--- src/common/types/chain.rs
+++ src/common/types/chain.rs
@@ -7,7 +7,9 @@ pub const OP_BEDROCK_CHAIN_IDS: &[u64] = &[
     Chain::BaseGoerli as u64,
 ];
 
-pub const ARBITRUM_CHAIN_IDS: &[u64] = &[Chain::Arbitrum as u64, Chain::ArbitrumGoerli as u64];
+// TODO use chain from ethers types once my PR is merged into ethers
+pub const ARBITRUM_CHAIN_IDS: &[u64] =
+    &[Chain::Arbitrum as u64, Chain::ArbitrumGoerli as u64, 421614];
 
 pub const BASE_CHAIN_IDS: &[u64] = &[8453, Chain::BaseGoerli as u64];
 
