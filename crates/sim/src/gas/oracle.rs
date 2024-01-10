7020e52b967a036c25fab648011e7376d0ce0ab8
dancoombs <dan.coombs@alchemy.com>
2024-01-10 09:59:16 -0500

    feat: use usage based oracle on polygon

--- crates/sim/src/gas/oracle.rs
+++ crates/sim/src/gas/oracle.rs
@@ -10,6 +10,7 @@
 //
 // You should have received a copy of the GNU General Public License along with Rundler.
 // If not, see https://www.gnu.org/licenses/.
+#![allow(dead_code)]
 
 use std::{fmt::Debug, sync::Arc};
 
