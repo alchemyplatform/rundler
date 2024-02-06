529ac84bc6ee527c357a0b7747866cf90ddd79de
JP <36560907+0xfourzerofour@users.noreply.github.com>
2024-02-06 09:33:38 -0500

    feat(chains): add polygon amoy and use alloy-rs (#583)

--- crates/types/src/chain.rs
+++ crates/types/src/chain.rs
@@ -13,37 +13,48 @@
 
 //! Grouped/Labeled chain IDs for various networks
 
-use ethers::types::Chain;
+use alloy_chains::NamedChain;
+use constcat::concat_slices;
 
 /// Known chain IDs that use the Optimism Bedrock stack
 pub const OP_BEDROCK_CHAIN_IDS: &[u64] = &[
-    Chain::Optimism as u64,
-    Chain::OptimismGoerli as u64,
-    11155420, // OptimismSepolia
-    Chain::Base as u64,
-    Chain::BaseGoerli as u64,
-    84532, // BaseSepolia
+    NamedChain::Optimism as u64,
+    NamedChain::OptimismGoerli as u64,
+    NamedChain::OptimismSepolia as u64,
+    NamedChain::Base as u64,
+    NamedChain::BaseGoerli as u64,
+    NamedChain::BaseSepolia as u64,
 ];
 
 // TODO use chain from ethers types once my PR is merged into ethers
 // https://github.com/gakonst/ethers-rs/pull/2657
 /// Known chain IDs for the Base ecosystem
 pub const ARBITRUM_CHAIN_IDS: &[u64] = &[
-    Chain::Arbitrum as u64,
-    Chain::ArbitrumGoerli as u64,
-    421614, /* ArbitrumSepolia */
-    Chain::ArbitrumNova as u64,
+    NamedChain::Arbitrum as u64,
+    NamedChain::ArbitrumGoerli as u64,
+    NamedChain::ArbitrumSepolia as u64,
+    NamedChain::ArbitrumNova as u64,
 ];
 
 /// Known chain IDs for the Base ecosystem
 pub const BASE_CHAIN_IDS: &[u64] = &[
-    Chain::Base as u64,
-    Chain::BaseGoerli as u64,
-    84532, /* BaseSepolia */
+    NamedChain::Base as u64,
+    NamedChain::BaseGoerli as u64,
+    NamedChain::BaseSepolia as u64,
 ];
 
 /// Known chain IDs for the Polygon ecosystem
-pub const POLYGON_CHAIN_IDS: &[u64] = &[Chain::Polygon as u64, Chain::PolygonMumbai as u64];
+pub const POLYGON_CHAIN_IDS: &[u64] =
+    concat_slices!([u64]: POLYGON_TESTNET_CHAIN_IDS, POLYGON_MAINNET_CHAIN_IDS);
+
+/// Known chain IDs for the Polygon ecosystem
+pub const POLYGON_TESTNET_CHAIN_IDS: &[u64] = &[
+    NamedChain::PolygonMumbai as u64,
+    80002, // PolygonAmoy - Change to named chain once there is a new release on alloy-rs/chains
+];
+
+/// Known chain IDs for the Polygon ecosystem
+pub const POLYGON_MAINNET_CHAIN_IDS: &[u64] = &[NamedChain::Polygon as u64];
 
 /// Return true if the chain ID has a dynamic preVerificationGas field
 pub fn is_dynamic_pvg(chain_id: u64) -> bool {
