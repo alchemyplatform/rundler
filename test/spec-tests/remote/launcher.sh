e311a8cf34ed9718e012a47fd1925ac96d6c9d1a
dancoombs <dan.coombs@alchemy.com>
2024-04-12 06:37:34 -0500

    chore(ci): fix remote tests for v0.6 and v0.7

--- test/spec-tests/remote/launcher.sh
+++ test/spec-tests/remote/launcher.sh
@@ -3,7 +3,23 @@
 
 export TAG=latest
 cd `dirname \`realpath $0\``
+
 case $1 in
+	v0_6)
+	export ENTRY_POINT_V0_6_ENABLED=true
+	export ENTRY_POINT_V0_7_ENABLED=false
+	;;
+	v0_7)
+	export ENTRY_POINT_V0_6_ENABLED=false
+	export ENTRY_POINT_V0_7_ENABLED=true
+	;;
+	*)
+	echo "usage: $0 {v0_6|v0_7} {start|stop|name}"
+	exit 1
+esac
+
+
+case $2 in
 
  name)
 	echo "rundler/$TAG"
@@ -12,12 +28,12 @@ case $1 in
  start)
 	docker-compose up -d --wait
 	cast send --unlocked --from $(cast rpc eth_accounts | tail -n 1 | tr -d '[]"') --value 1ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 > /dev/null
-    cd ../bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
+    cd ../$1/bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
 	;;
  stop)
  	docker-compose down -t 3
 	;;
 
  *)
-	echo "usage: $0 {start|stop|name}"
+	echo "usage: $0 {v0_6|v0_7} {start|stop|name}"
 esac
