976ceeacef6f627fbb0e748aeb2a76165d8a7fbe
David Philipson <david.philipson@gmail.com>
2024-04-16 15:07:07 -0700

    chore(docs): Update commands in makefile, readme, and help messages

--- test/spec-tests/remote/launcher.sh
+++ test/spec-tests/remote/launcher.sh
@@ -2,38 +2,44 @@
 # Launcher script for the rundler.
 
 export TAG=latest
+export ENTRY_POINT_V0_6_ENABLED=false
+export ENTRY_POINT_V0_7_ENABLED=false
 cd `dirname \`realpath $0\``
-
 case $1 in
-	v0_6)
-	export ENTRY_POINT_V0_6_ENABLED=true
-	export ENTRY_POINT_V0_7_ENABLED=false
-	;;
-	v0_7)
-	export ENTRY_POINT_V0_6_ENABLED=false
-	export ENTRY_POINT_V0_7_ENABLED=true
-	;;
-	*)
-	echo "usage: $0 {v0_6|v0_7} {start|stop|name}"
-	exit 1
-esac
-
-
-case $2 in
 
  name)
 	echo "rundler/$TAG"
 	;;
 
  start)
+	case $2 in
+		v0_6)
+		export ENTRY_POINT_V0_6_ENABLED=true
+		;;
+		v0_7)
+		export ENTRY_POINT_V0_7_ENABLED=true
+		;;
+		*)
+    cat <<EOF
+usage:
+  $0 start {v0_6|v0_7}
+  $0 stop
+EOF
+		exit 1
+	esac
 	docker-compose up -d --wait
 	cast send --unlocked --from $(cast rpc eth_accounts | tail -n 1 | tr -d '[]"') --value 1ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 > /dev/null
-    cd ../$1/bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
+    cd ../$2/bundler-spec-tests/@account-abstraction && yarn deploy --network localhost
 	;;
  stop)
  	docker-compose down -t 3
 	;;
 
  *)
-	echo "usage: $0 {v0_6|v0_7} {start|stop|name}"
+  cat <<EOF
+usage:
+  $0 name
+  $0 start {v0_6|v0_7}
+  $0 stop
+EOF
 esac
