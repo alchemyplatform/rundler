98f8e2ccd1da6513822e6f76097c3a6624cc6129
JP <36560907+0xfourzerofour@users.noreply.github.com>
2024-06-14 14:38:20 -0400

    feat(ci): check if output file exists (#727)

--- .github/scripts/compliance_parser.sh
+++ .github/scripts/compliance_parser.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check for xml output file 
+if [ -f "$1" ]; then
+    echo "$1 exists."
+else 
+    echo "$1 does not exist."
+    exit 1
+fi
 # Parse the XML data
 errors=$(xmllint --xpath 'string(//testsuite/@errors)' "$1")
 failures=$(xmllint --xpath 'string(//testsuite/@failures)' "$1")
