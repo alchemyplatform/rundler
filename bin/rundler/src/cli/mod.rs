bfd9064ff05441417e4de689ae99f34895f6ed04
0xfourzerofour <joshpauline@gmail.com>
2024-09-13 12:37:19 -0400

    feat(metrics): update histogram buckets and time to mine value

--- bin/rundler/src/cli/mod.rs
+++ bin/rundler/src/cli/mod.rs
@@ -450,9 +450,13 @@ pub struct MetricsArgs {
         long = "metrics.histogram_buckets", 
         name = "metrics.histogram_buckets",
         env = "METRICS_HISTOGRAM_BUCKETS",
-        default_values_t = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,
-                                9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
-                                32.0, 64.0, 128.0, 256.0, 512.0],
+        default_values_t = vec![
+            // Fine-grained buckets from 0 to 1000
+            0.0, 50.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0,
+            500.0, 600.0, 700.0, 800.0, 900.0, 1000.0,
+            // Coarser buckets from 1000 to 10,000
+            2000.0, 3000.0, 4000.0, 5000.0, 7500.0, 10000.0,
+        ],
         global = true
     )]
     buckets: Vec<f64>,
