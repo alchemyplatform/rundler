de3391507abb98948e6dbfb1d88417214c48760c
dancoombs <dan.coombs@alchemy.com>
2024-09-18 12:14:32 -0500

    feat(contracts): add contracts crate, remove from types

--- crates/contracts/contracts/src/utils/GetCodeHashes.sol
+++ crates/contracts/contracts/src/utils/GetCodeHashes.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.13;
 // From eth-infinitism/bundler. A helper contract for hashing together the code
 // hashes of multiple contracts at once.
 //
-// Not intended to be deployed on-chain.. Instead, using a call to simulate
-// deployment will revert with an error containing the desired result.
+// Not intended to be deployed on-chain.. Instead use state overrides to deploy the bytecode and call it.
 
 contract GetCodeHashes {
-    error CodeHashesResult(bytes32 hash);
-
-    constructor(address[] memory addresses) {
-        revert CodeHashesResult(getCodeHashes(addresses));
+    /**
+     * Contract should not be deployed
+     */
+    constructor() {
+        require(block.number < 100, "should not be deployed");
     }
 
     function getCodeHashes(
         address[] memory addresses
-    ) public view returns (bytes32) {
+    ) public view returns (bytes32 hash) {
         bytes32[] memory hashes = new bytes32[](addresses.length);
         for (uint i = 0; i < addresses.length; i++) {
             hashes[i] = addresses[i].codehash;
