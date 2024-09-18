0f006e9ca0634fe2df08722b19a7606ef000d1aa
dancoombs <dan.coombs@alchemy.com>
2024-09-18 12:14:32 -0500

    feat(contracts): add contracts crate, remove from types

--- crates/contracts/contracts/src/v0_7/imports.sol
+++ crates/contracts/contracts/src/v0_7/imports.sol
@@ -8,5 +8,6 @@ import "account-abstraction/v0_7/interfaces/IAccount.sol";
 import "account-abstraction/v0_7/interfaces/IPaymaster.sol";
 import "account-abstraction/v0_7/interfaces/IAggregator.sol";
 import "account-abstraction/v0_7/interfaces/IStakeManager.sol";
+import "account-abstraction/v0_7/interfaces/PackedUserOperation.sol";
 import "account-abstraction/v0_7/core/EntryPointSimulations.sol";
 import "account-abstraction/v0_7/core/SenderCreator.sol";
