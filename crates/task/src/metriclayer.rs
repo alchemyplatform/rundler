1060c2e89b0c6cf3ca77fb2424d94ea4be31e63c
Pengfei(Andy) Zhang <andy@alchemy.com>
2024-09-19 16:45:07 -0400

    feat(middleware): remove hashmap to async lock-free impl

--- crates/task/src/metriclayer.rs
+++ crates/task/src/metriclayer.rs
@@ -3,13 +3,14 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
     time::{Duration, Instant},
-    sync::{Arc, Mutex},
 };
 
 use jsonrpsee::server::HttpBody;
 use pin_project::pin_project;
 use tower::{Layer, Service};
 use tonic::codegen::http;
+
+#![allow(dead_code)]
 
 pub fn get_method_name(request : http::Request<Body>) -> String {
     let uri = request.uri();
@@ -20,7 +21,6 @@ pub fn get_method_name(request : http::Request<Body>) -> String {
 pub fn get_method_name<'a>(request : jsonrpsee::types::Request<'a>) -> String {
     request.method_name().to_string()
 }
-
 #[derive(Debug, Clone)]
 pub struct MetricsLayer {
     service_name: String,
@@ -66,7 +66,6 @@ pub struct ResponseFuture<F> {
     start_time: Instant,
     method_name: String,
     service_name: String,
-    method_metric: Arc<Mutex<MethodMetrics>>,
 }
 
 impl<S> Service<RequestInfo> for MetricsMiddleware<S>
@@ -84,19 +83,16 @@ where
     fn call(&mut self, request: http::Request<Body>) -> Self::Future {
         let uri = request.uri().clone();
         let method_name = uri.path().split('/').last().unwrap_or("unknown");
-        let mut method_metric = self.service_metrics.get_method_metrics(method_name);
-
         {
             let mut metric = method_metric.lock().unwarp();
-            metric.increment_num_requests();
-            metric.increment_open_requests();
+            MethodMetrics::increment_num_requests(self.service_name.as_str(), method_name);
+            MethodMetrics::increment_open_requests(self.service_name.as_str(), method_name);
         }
         ResponseFuture{
             response_future: self.inner.call(request),
             start_time: Instant::now(),
             method_name: method_name.to_string(),
             service_name: self.service_name.clone(),
-            method_metric: method_metric.clone(),
         }
     }
 }
@@ -111,15 +107,12 @@ where
         let this = self.project();
         let res = this.response_future.poll(cx);
 
-        // Get the duration here so it won't count the time of acquiring lock.
-        let duration = this.start_time.elapsed();
-        let mut metric = self.method_metric.lock().unwrap();
         if res.is_ready() {
-            metric.decrement_open_requests();
-            metric.record_request_latency(duration);
+            MethodMetrics::decrement_open_requests(self.service_name.as_str(), method_name);
+            MethodMetrics::record_request_latency(self.service_name.as_str(), method_name, this.start_time.elapsed());
         }
         if let Poll::Ready(Err(_)) = res {
-            metric.increment_error_count();
+            MethodMetrics::increment_error_count(self.service_name.as_str(), method_name);
         }
         res
     }
@@ -133,93 +126,42 @@ pub struct ResponseFuture<F> {
     method_name: String,
 }
 
-impl<F, Response, Error> Future for ResponseFuture<F>
-where
-    F: Future<Output = Result<Response, Error>>,
-{
-    type Output = Result<Response, Error>;
-
-    fn poll(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Self::Output> {
-        let this = self.project();
-        let res = this.response_future.poll(cx);
-        if res.is_ready() {
-            MetricsRecorder::decrement_open_requests(this.method_name, this.service_name);
-            MetricsRecorder::record_request_latency(
-                this.method_name,
-                this.service_name,
-                this.start_time.elapsed(),
-            );
-        }
-
-        if let Poll::Ready(Err(_)) = res {
-            MetricsRecorder::increment_rpc_error_count(this.method_name, this.service_name);
-        }
-
-        res
-    }
-}
-
 
 #[derive(Clone)]
 // service metrics tracks all method metrics of specific service.
 struct ServiceMetrics {
     service_name: &str,
-    method_metrics: HashMap<&'static str, Arc<Mutex<MethodMetrics>>>,
 }
 
 impl ServiceMetrics {
     fn new(service_name: &str) -> Self {
         Self {
-            service_name: service_name,
-            method_metrics: HashMap::new(),
+            service_name: service_name
         }
-    }
-
-    fn get_method_metrics(mut self, method_name: &str) -> Arc<Mutex<MethodMetrics>> {
-        self.method_metrics
-            .entry(&method_name)
-            .or_insert(
-                Arc::new(Mutex::new(MethodMetrics::new(self.service_name, method_name))))
     }
 }
 
 #[derive(Clone)]
-struct MethodMetrics {
-    num_requests: Counter,
-    open_requests: Gauge,
-    error_count: Counter,
-    request_latency: Histogram,
-}
+struct MethodMetrics {}
 
 impl MethodMetrics {
-    fn new(service_name: &str, method_name: &str) -> Self {
-        Self {
-            num_requests: metrics::counter!("num_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()),
-            open_requests: metrics::gauge!("open_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()),
-            error_count: metrics::counter!("open_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()),
-            request_latency: metrics::histogram!("request_latency", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()),
-        }
-    }
-    fn increment_num_requests(&self) {
-        self.num_requests.increment(1)
+    fn increment_num_requests(&self, method_name: String, service_name: String) {
+        metrics::counter!("num_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()).increment(1)
     }
 
-    fn increment_open_requests(&self) {
-        self.open_requests.increment(1_f64)
+    fn increment_open_requests(&self, method_name: String, service_name: String) {
+        metrics::gauge!("open_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()).increment(1_f64)
     }
 
-    fn decrement_open_requests(&self) {
-        self.open_requests.decrement(1_f64)
+    fn decrement_open_requests(&self, method_name: String, service_name: String) {
+        metrics::gauge!("open_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()).decrement(1_f64)
     }
 
-    fn increment_error_count(&self) {
-        self.error_count.increment(1)
+    fn increment_error_count(&self, method_name: String, service_name: String) {
+        metrics::counter!("open_requests", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()).increment(1)
     }
 
-    fn record_request_latency(&self, latency: Duration) {
-        self.request_latency.record(latency)
+    fn record_request_latency(&self, , method_name: String, service_name: String, latency: Duration) {
+        metrics::histogram!("request_latency", "method_name" => method_name.to_string(), "service_name" => service_name.to_string()).record(latency)
     }
 }
