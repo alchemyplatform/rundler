3ee8212fec44e958626ac08d9fa8c6f007f3cab4
dancoombs <dan.coombs@alchemy.com>
2025-03-04 19:55:45 -0600

    feat(rpc): add rundler_getMinedUserOperation

--- crates/rpc/src/rundler.rs
+++ crates/rpc/src/rundler.rs
@@ -14,14 +14,16 @@
 use alloy_primitives::{Address, B256, U128};
 use anyhow::Context;
 use async_trait::async_trait;
+use futures_util::future;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use rundler_provider::EvmProvider;
 use rundler_sim::{gas, FeeEstimator};
 use rundler_types::{chain::ChainSpec, pool::Pool, UserOperation, UserOperationVariant};
 use tracing::instrument;
 
 use crate::{
     eth::{EntryPointRouter, EthResult, EthRpcError},
-    types::{FromRpc, RpcUserOperation},
+    types::{FromRpc, RpcMinedUserOperation, RpcUserOperation},
     utils,
 };
 
@@ -55,20 +57,31 @@ pub trait RundlerApi {
         uo: RpcUserOperation,
         entry_point: Address,
     ) -> RpcResult<Option<B256>>;
+
+    /// Gets information about a mined user operation by user operation hash, transaction hash, and entrypoint address
+    #[method(name = "getMinedUserOperation")]
+    async fn get_mined_user_operation(
+        &self,
+        uo_hash: B256,
+        tx_hash: B256,
+        entry_point: Address,
+    ) -> RpcResult<Option<RpcMinedUserOperation>>;
 }
 
-pub(crate) struct RundlerApi<P, F> {
+pub(crate) struct RundlerApi<P, F, E> {
     chain_spec: ChainSpec,
     fee_estimator: F,
     pool_server: P,
     entry_point_router: EntryPointRouter,
+    evm_provider: E,
 }
 
 #[async_trait]
-impl<P, F> RundlerApiServer for RundlerApi<P, F>
+impl<P, F, E> RundlerApiServer for RundlerApi<P, F, E>
 where
     P: Pool + 'static,
     F: FeeEstimator + 'static,
+    E: EvmProvider + 'static,
 {
     #[instrument(skip_all, fields(rpc_method = "rundler_maxPriorityFeePerGas"))]
     async fn max_priority_fee_per_gas(&self) -> RpcResult<U128> {
@@ -91,24 +104,41 @@ where
         )
         .await
     }
+
+    #[instrument(skip_all, fields(rpc_method = "rundler_getMinedUserOperation"))]
+    async fn get_mined_user_operation(
+        &self,
+        uo_hash: B256,
+        tx_hash: B256,
+        entry_point: Address,
+    ) -> RpcResult<Option<RpcMinedUserOperation>> {
+        utils::safe_call_rpc_handler(
+            "rundler_getMinedUserOperation",
+            RundlerApi::get_mined_user_operation(self, uo_hash, tx_hash, entry_point),
+        )
+        .await
+    }
 }
 
-impl<P, F> RundlerApi<P, F>
+impl<P, F, E> RundlerApi<P, F, E>
 where
     P: Pool,
     F: FeeEstimator,
+    E: EvmProvider,
 {
     pub(crate) fn new(
         chain_spec: &ChainSpec,
         entry_point_router: EntryPointRouter,
         pool_server: P,
         fee_estimator: F,
+        evm_provider: E,
     ) -> Self {
         Self {
             chain_spec: chain_spec.clone(),
             entry_point_router,
             pool_server,
             fee_estimator,
+            evm_provider,
         }
     }
     #[instrument(skip_all)]
@@ -162,5 +192,43 @@ where
             })?;
 
         Ok(ret)
+    }
+
+    async fn get_mined_user_operation(
+        &self,
+        uo_hash: B256,
+        tx_hash: B256,
+        entry_point: Address,
+    ) -> EthResult<Option<RpcMinedUserOperation>> {
+        let tx_receipt = self
+            .evm_provider
+            .get_transaction_receipt(tx_hash)
+            .await
+            .context("should have fetched tx receipt")?
+            .context("Failed to fetch tx receipt")?;
+
+        let receipt_fut = self.entry_point_router.get_receipt_from_tx_receipt(
+            &entry_point,
+            uo_hash,
+            tx_receipt.clone(),
+        );
+        let uo_fut =
+            self.entry_point_router
+                .get_mined_from_tx_receipt(&entry_point, uo_hash, tx_receipt);
+
+        let (uo, receipt) = future::try_join(uo_fut, receipt_fut).await?;
+
+        match (uo, receipt) {
+            (Some(uo), Some(mut receipt)) => {
+                // clear out the outer transaction logs from the receipt to avoid sending
+                // extra data in the response. OOF - this type is a mess.
+                receipt.receipt.inner.inner.receipt.logs.clear();
+                Ok(Some(RpcMinedUserOperation {
+                    user_operation: uo.user_operation,
+                    receipt,
+                }))
+            }
+            _ => Ok(None),
+        }
     }
 }
